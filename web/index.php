63ac39dca813479cb347e98e475939eb008b6a27
Michael Müller <mishbag@reu-network.de>
2016-09-17 23:46:59 +0200

    added impressum

--- web/index.php
+++ web/index.php
@@ -5,9 +5,22 @@
  /*************************************************************
   * RENDER LAYOUT
   */
- $view = "home.php";
- $variables = array(
- );
+ $page = null;
+ if(isset($_REQUEST['page']))
+ {
+ 	$page = $_REQUEST['page'];
+ }
+ 
+ switch ($page)
+ {
+ 	case 'impressum':
+ 		$view = "impressum.php";
+ 		$variables = array();
+ 		break;
+ 	default:
+		 $view = "home.php";
+		 $variables = array();
+ }
  
  renderLayout($view, $variables);
 ?>
