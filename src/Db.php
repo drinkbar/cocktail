90fc0165103239dca5f8c95a05fc0d6a5e47592e
Michael Müller <mishbag@reu-network.de>
2016-09-18 09:22:35 +0200

    created database class

---
+++ src/Db.php
@@ -0,0 +1,47 @@
+<?php
+class Db {
+	protected static $connection;
+	
+	public function connect()
+	{
+		if(!isset(self::$connection))
+		{
+			// parse config file
+			$config = parse_ini_file('../resources/config/config.ini');
+			// connect
+			self::$connection = new mysqli($config['db_host'], $config['db_user'], $config['db_passwd'], $config['db_name']);
+			var_dump(self::$connection);
+			// check connection
+			if(self::$connection === false)
+				die("Verbindung zur Datenbank konnte nicht hergestellt werden");
+			else
+				return self::$connection;
+		}
+	}
+	
+	public function query($query)
+	{
+		// connect
+		$con = $this->connect();
+		
+		$result = $con->query($query);
+		
+		return $result;
+	}
+	
+	public function select($query)
+	{
+		$rows = array();
+		$result = $this->query($query);
+		
+		if($result === false) 
+			return false;
+		
+		while ($row = $result->fetch_assoc()) 
+		{
+			$rows[] = $row;
+		}
+		
+		return $rows;	
+	}
+}
