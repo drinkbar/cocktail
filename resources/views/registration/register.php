58f11bf2142922c7d57624fee357f3e6df39cf29
Michael Müller <mishbag@reu-network.de>
2016-09-18 12:32:19 +0200

    included registration controller

---
+++ resources/views/registration/register.php
@@ -0,0 +1,10 @@
+<?php 
+require_once '../src/Db.php';
+?>
+<main>
+	<div class="row">
+		<hr />
+		<h5>Mitglied werden</h5>
+		<p>Hier wird das Registrierungsformular angezeigt.</p>
+	</div>
+</main>
--- web/registration.php
+++ web/registration.php
@@ -1,1 +1,14 @@
 <?php
+require_once('../resources/config/core.php');
+require_once(LIBRARY_PATH . '/templateFunctions.php');
+
+/*************************************************************
+ * RENDER LAYOUT
+ */
+$view = "registration/register.php";
+$variables = array();
+
+// handle session parameter
+require_once(LIBRARY_PATH . '/session.php');
+
+renderLayout($view, $variables);
