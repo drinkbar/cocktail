6b14f5ea8d2f9ff912782ab8e0784d81d92607d3
Michael Müller <mishbag@reu-network.de>
2016-09-18 12:13:19 +0200

    improved view

--- resources/views/cocktail/show.php
+++ resources/views/cocktail/show.php
@@ -1,7 +1,51 @@
+<?php 
+require_once '../src/Db.php';
+
+$db = new Db();
+$cocktail = $db->select("SELECT *
+						FROM cocktail c 
+						WHERE c.ID = $id");
+
+$zutaten = $db->select("SELECT z.*, cz.Menge
+						FROM cocktail c 
+						 JOIN cocktail_zutat cz ON (cz.Cocktail_ID = c.ID) 
+						 JOIN zutat z ON (z.ID = cz.Zutat_ID) 
+						WHERE c.ID = $id");
+
+if(!empty($cocktail))
+{
+	$name = $cocktail[0]['Cocktailname'];
+	//$zutaten = $cocktail[0]['Zutaten'];
+	$rezept = $cocktail[0]['Rezept'];
+	$image = $cocktail[0]['Bild'];
+}
+else
+{
+	session_start();
+	$_SESSION['error'] = "Es wurde ein ungültiger Cocktail angegeben.";
+	header("Location: cocktail.php");
+}
+?>
 <main>
 	<div class="row">
 		<hr />
-		<h5>Cocktail Details</h5>
-		<p>Hier werden die Details zu einem bestimmten Cocktail angezeigt.</p>
+		<h5><?php echo $name; ?></h5>
+		<div class="small-6 columns">
+			<img src="<?php echo $image; ?>" alt="<?php echo $name; ?>" title="<?php echo $name; ?>" />
+		</div>
+		<div class="small-6 columns">
+			<h3>Zutaten</h3>
+			<ul>
+				<?php 
+				foreach ($zutaten as $zutat)
+				{
+					echo "<li>";
+					echo $zutat['Menge']." ".$zutat['Bezeichnung'];
+					echo "</li>";
+				}?>
+			</ul>		
+			<h3>Rezept</h3>
+			<p><?php echo $rezept; ?></p>
+		</div>
 	</div>
 </main>
