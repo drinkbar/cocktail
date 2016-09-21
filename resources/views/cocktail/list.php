142807245b7649954cbc32675954b41eb076476c
Michael Müller <mishbag@reu-network.de>
2016-09-21 09:48:58 +0200

    included cocktail widget

--- resources/views/cocktail/list.php
+++ resources/views/cocktail/list.php
@@ -8,16 +8,14 @@ $list = $db->select("SELECT * FROM cocktail WHERE 1 ORDER BY Cocktailname");
 	<div class="row">
 		<hr />
 		<h5>Cocktails</h5>
-		<p>Hier werden alle Cocktails aufgelistet.</p>
-		<ul>
 			<?php 
 			foreach ($list as $cocktail)
 			{
-				echo "<li>";
-				echo "<a href=\"cocktail.php?show=".$cocktail['ID']."\">".$cocktail['Cocktailname']."</a>";
-				echo "</li>";
+				include 'cocktail_widget.php';
+				//echo "<li>";
+				//echo "<a href=\"cocktail.php?show=".$cocktail['ID']."\">".$cocktail['Cocktailname']."</a>";
+				//echo "</li>";
 			}
 			?>
-		</ul>
 	</div>
 </main>
