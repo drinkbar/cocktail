13eb6c44dc2248ecb03600fdfae122529e18a816
Michael Müller <mishbag@reu-network.de>
2016-09-22 09:58:26 +0200

    added new top menu

--- resources/views/template/header.php
+++ resources/views/template/header.php
@@ -16,41 +16,34 @@
 <header>
 	<div class="row" >
 		<div class ="large-12 columns">
-			<div class="pc">	
-				<?php 
-					if(isset($_SESSION['login']) && $_SESSION['login'] == 1)
-					{
-						echo "<ul class=\"menu align-right\">
-							  <li><a href=\"security.php?logout\" class=\"button\">Logout</a></li>
-							  <li>&nbsp;</li>
-							  <li>Willkommen $username</li>
-							</ul>";
-					}
-					else 
-						include(TEMPLATE_PATH . "/login_form.html");
-					
-					if(isset($error))
-					{
-						echo "
-						<div data-alert class=\"callout alert\">$error</div>";
-					}
-					if(isset($warning))
-					{
-						echo "
-						<div data-alert class=\"callout warning\">$warning</div>";
-					}
-					if(isset($success))
-					{
-						echo "
-						<div data-alert class=\"callout success\">$success</div>";
-					}
-					?>	
-			</div>
+			<?php 
+				if(isset($_SESSION['login']) && $_SESSION['login'] == 1)
+				{
+					include TEMPLATE_PATH . "/user_nav.php";
+				}
+				else 
+					include TEMPLATE_PATH . "/login_form.html";
+				
+				if(isset($error))
+				{
+					echo "
+					<div data-alert class=\"callout alert\">$error</div>";
+				}
+				if(isset($warning))
+				{
+					echo "
+					<div data-alert class=\"callout warning\">$warning</div>";
+				}
+				if(isset($success))
+				{
+					echo "
+					<div data-alert class=\"callout success\">$success</div>";
+				}
+				?>	
 		</div>
 		
 		<div class="large-12 columns">
 			<h1>Drinkbar</h1>
-			<div class="pc">
 				<div class="menu-centered">
 					<ul class="menu">
 						<li><a href="index.php">Home</a></li>
@@ -61,30 +54,7 @@
 						<?php  if(isset($_SESSION['user'])) echo "<li><a href=\"index.php?seite=einkaufsliste\">Einkaufsliste</a></li>"; ?>
 			   		</ul>
 		   		</div>
-	   		</div>
    		</div>
 		<hr />
 	</div> 
-		 
-	<div class="smart">
-		<div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
-			<button class="menu-icon" type="button" data-toggle></button>
-			<div class="title-bar-title">Navigation</div>
-		</div>
-		
-		<div class="top-bar" id="example-menu">
-			<div class="top-bar-left">
-				<ul class="menu vertical">
-					<li><a href="index.php?seite=startseite">Home</a></li>
-					<li><a href="index.php?seite=cocktails">Cocktails</a></li>
-					<li><a href="index.php?seite=zutaten">Zutaten</a></li>
-					<li><a href="index.php?seite=bestenliste">Bestenliste</a></li>
-					<li><a href="index.php?seite=suche">Suche</a></li>
-					<li><a href="index.php?seite=einkaufsliste">Einkaufsliste</a></li>
-					<li> <a href="index.php?seite=anmelden">Mitglied werden</a></li>
-					<li><a href="index.php?seite=login">Login</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
 </header>
---
+++ resources/views/template/user_nav.php
@@ -0,0 +1,22 @@
+<div class="title-bar" data-responsive-toggle="user-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle></button>
+  <div class="title-bar-title">Benutzernavigation</div>
+</div>
+
+<div class="top-bar" id="user-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text"><?php echo $username; ?></li>
+      <li>
+        <a href="#">Meine Zutaten</a>
+      </li>
+      <li><a href="favoriten.php">Meine Favoriten</a></li>
+      <li><a href="#">Partyplaner</a></li>
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><a href="security.php?logout" class="button secondary">Logout</a></li>
+    </ul>
+  </div>
+</div>
